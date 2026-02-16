09da427800f4c7f24d1f9377da3f1d488e9fbb33
Otto Sellerstam <otto.sellerstam@analyzera.com>
2026-02-16 14:25:48 +0100

    Updating docstrings in one-ring-core's result types

--- one-ring-core/src/one_ring_core/results.py
+++ one-ring-core/src/one_ring_core/results.py
@@ -1,51 +1,67 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
-from one_ring_core.types import UserData
+if TYPE_CHECKING:
+    from one_ring_core.types import UserData
 
 
 @dataclass(frozen=True)
 class IOCompletion:
+    """Return type for CQE completions."""
+
     user_data: UserData
     result: IOResult
 
+
 @dataclass(frozen=True)
 class IOResult:
-    """Base class for all IO operation results"""
+    """Base class for all IO operation results."""
+
 
 @dataclass(frozen=True)
 class FileIOResult(IOResult):
-    """Base class for all file IO operation results"""
+    """Base class for all file IO operation results."""
+
 
 @dataclass(frozen=True)
 class NetworkIOResult(IOResult):
-    """Base class for all networking IO operation results"""
+    """Base class for all networking IO operation results."""
+
 
 @dataclass(frozen=True)
 class TimerIOResult(IOResult):
-    """Base class for all timer IO operation results"""
+    """Base class for all timer IO operation results."""
+
 
 @dataclass(frozen=True)
 class ControlIOResult(IOResult):
-    """Base class for all control IO operation results"""
+    """Base class for all control IO operation results."""
+
 
 @dataclass(frozen=True)
 class FileOpenResult(IOResult):
-    """Result of a file open operation"""
+    """Result of a file open operation."""
+
     fd: int
+
 
 @dataclass(frozen=True)
 class FileReadResult(IOResult):
-    """Result of a file read operation"""
+    """Result of a file read operation."""
+
     content: str
     size: int
 
+
 @dataclass(frozen=True)
 class FileWriteResult(IOResult):
-    """Result of a file write operation"""
+    """Result of a file write operation."""
+
     size: int
+
 
 @dataclass(frozen=True)
 class FileCloseResult(IOResult):
-    """Result of a file close operation"""
+    """Result of a file close operation."""
